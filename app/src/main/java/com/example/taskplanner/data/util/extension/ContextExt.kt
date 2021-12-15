0831a940dc3957d141832144577c78597cee3c28
GiorgiMamagulashvili16 <giorgi.mamagulashvili.1@btu.edu.ge>
2021-12-15 16:23:19 +0400

    changed getStatus ext

--- app/src/main/java/com/example/taskplanner/data/util/extension/ContextExt.kt
+++
@@ -1,8 +0,0 @@
-package com.example.taskplanner.data.util.extension
-
-import android.content.Context
-import com.example.taskplanner.presentation.project_screen.Status
-
-fun Context.getStatusByOrdinal(ordinal: Int): Status {
-    return Status.values()[ordinal]
-}
---
+++ app/src/main/java/com/example/taskplanner/data/util/extension/IntExt.kt
@@ -0,0 +1,7 @@
+package com.example.taskplanner.data.util.extension
+
+ import com.example.taskplanner.presentation.project_screen.Status
+
+fun Int.getStatusByOrdinal(): Status {
+    return Status.values()[this]
+}
